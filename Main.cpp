c451027f03ea30e513e1cfdceffdfb3684bdeb3c
TiffannyVarela <tiffanny_varela529@unitec.edu>
2020-03-13 17:04:26 -0600

    Octavo

--- Main.cpp
+++ Main.cpp
@@ -8,20 +8,23 @@ using namespace std;
 
 int menu();
 int menuTipos();
+int menuOpera();
 	vector<Vector<Racional>> vectores_R;
 	vector<Vector<Complejo>> vectores_C;
-void print();
+void printR();
+void printC();
 int MCD(int,int);
 
 int main(int argc, char const *argv[])
 {
 	int resp=-1;
-	int opc=0, opc2=0;
+	int opc=0, opc2=0, opc3=0, opc4=0;
 	int num,den;
 	int real,imag;
 	Racional r1,r2,r3;
 	Complejo c1,c2,c3;
 	int mcd;
+	int pos1, pos2;
 	do//inicio do while
 	{
 		switch(opc=menu()){//inicio switch
@@ -29,47 +32,18 @@ int main(int argc, char const *argv[])
 			case 1://inicio case 1
 				switch(opc2=menuTipos()){//inicio switch2
 
-					case 1:{//inicio case 1
-						for (int i = 0; i<3; ++i)
-						{
-							cout<<"Ingrese Numerador "<<i+1<<":";
-							cin>>num;
-							cout<<"Ingrese Denominador "<<i+1<<":";
-							cin>>den;
-							while(den==0){
-								cout<<"0 no es valido"<<endl;
-								cout<<"Ingrese Denominador "<<i+1<<":";
-								cin>>den;
-							}
-							mcd = MCD(num,den);
-							num/= mcd;
-							den/= mcd;
-							if(i==1){
-								r1 = Racional(num,den);
-							}
-							if(i==2){
-								r2 = Racional(num,den);
-							}
-							else{
-								r3 = Racional(num,den);
-							}
-						}
-						Vector <Racional> v(r1,r2,r3);
-						vectores_R.push_back(v);
-					}
-					break;//fin case 1
-
-					case 2:{//inicio case 2
+					case 1:{
+					//inicio case 1
 						for (int i = 0; i<3; ++i)
 							{
-								cout<<"Ingrese Real "<<i+1<<":";
+								cout<<"\n\nIngrese Real "<<i+1<<":";
 								cin>>real;
 								cout<<"Ingrese Imaginario "<<i+1<<":";
 								cin>>imag;
-								if(i==1){
+								if(i==0){
 									c1 = Complejo(real,imag);
 								}
-								if(i==2){
+								if(i==1){
 									c2 = Complejo(real,imag);
 								}
 								else{
@@ -79,16 +53,131 @@ int main(int argc, char const *argv[])
 							Vector <Complejo> c(c1,c2,c3);
 							vectores_C.push_back(c);
 					}
+					break;//fin case 1
+
+					case 2:{//inicio case 2
+						for (int i = 0; i<3; ++i)
+						{
+							cout<<"\n\nIngrese Numerador "<<i+1<<":  ";
+							cin>>num;
+							cout<<"Ingrese Denominador "<<i+1<<":  ";
+							cin>>den;
+							cout<<endl;
+							while(den==0){
+								cout<<"0 no es valido"<<endl;
+								cout<<"Ingrese Denominador "<<i+1<<":  ";
+								cin>>den;
+							}
+							
+							if(i==0){
+								mcd = MCD(num,den);
+								num= num/mcd;
+								den= den/mcd;
+								r1 = Racional(num,den);
+							}
+							if(i==1){
+								mcd = MCD(num,den);
+								num= num/mcd;
+								den= den/mcd;
+								r2 = Racional(num,den);
+							}
+							else{
+								mcd = MCD(num,den);
+								num= num/mcd;
+								den= den/mcd;
+								r3 = Racional(num,den);
+							}
+						}
+						Vector <Racional> v(r1,r2,r3);
+						vectores_R.push_back(v);
+					}
+						
 					break;//fin case 2
 
 				}//fin switch2
 			break;//fin case 1
 
 			case 2://inicio case 2
-				print();
+				printR();
+				printC();
 			break;//fin case 2
 
 			case 3://inicio case 3
+				switch(opc3=menuOpera()){
+
+					case 1:
+						switch(opc4=menuTipos()){
+							case 1:
+								printC();
+								cout<<"\nIngrese la posicion del primer numero: ";
+								cin>>pos1;
+								cout<<"\nIngrese la posicion del segundo numero: ";
+								cin>>pos2;
+								while(pos1<0 || pos1>numeros.size()-1 || pos2<0 || pos2>numeros.size()-1 ){
+									cout<<"\nINGRESE POSICIONES VALIDAS\n"<<endl;
+									printVector();
+									cout<<"\nIngrese la posicion del primer numero: ";
+									cin>>pos1;
+									cout<<"\nIngrese la posicion del segundo numero: ";
+									cin>>pos2;
+								}
+								
+							break;
+
+							case 2:
+								printR();
+								cout<<"\nIngrese la posicion del primer numero: ";
+								cin>>pos1;
+								cout<<"\nIngrese la posicion del segundo numero: ";
+								cin>>pos2;
+								while(pos1<0 || pos1>numeros.size()-1 || pos2<0 || pos2>numeros.size()-1 ){
+									cout<<"\nINGRESE POSICIONES VALIDAS\n"<<endl;
+									printVector();
+									cout<<"\nIngrese la posicion del primer numero: ";
+									cin>>pos1;
+									cout<<"\nIngrese la posicion del segundo numero: ";
+									cin>>pos2;
+								}
+							break;
+						}
+					break;
+
+					case 2:
+						switch(opc4=menuTipos()){
+							case 1:
+								printC();
+								cout<<"\nIngrese la posicion del primer numero: ";
+								cin>>pos1;
+								cout<<"\nIngrese la posicion del segundo numero: ";
+								cin>>pos2;
+								while(pos1<0 || pos1>numeros.size()-1 || pos2<0 || pos2>numeros.size()-1 ){
+									cout<<"\nINGRESE POSICIONES VALIDAS\n"<<endl;
+									printVector();
+									cout<<"\nIngrese la posicion del primer numero: ";
+									cin>>pos1;
+									cout<<"\nIngrese la posicion del segundo numero: ";
+									cin>>pos2;
+								}
+							break;
+
+							case 2:
+								printR();
+								cout<<"\nIngrese la posicion del primer numero: ";
+								cin>>pos1;
+								cout<<"\nIngrese la posicion del segundo numero: ";
+								cin>>pos2;
+								while(pos1<0 || pos1>numeros.size()-1 || pos2<0 || pos2>numeros.size()-1 ){
+									cout<<"\nINGRESE POSICIONES VALIDAS\n"<<endl;
+									printVector();
+									cout<<"\nIngrese la posicion del primer numero: ";
+									cin>>pos1;
+									cout<<"\nIngrese la posicion del segundo numero: ";
+									cin>>pos2;
+								}
+							break;
+						}
+					break;
+				}
 			break;//fin case 3
 
 		}//fin switch
@@ -137,7 +226,25 @@ int menuTipos(){//inicio metodo menu
         return 0;
 }//fin metodo menu
 
-void print(){
+int menuOpera(){//inicio metodo menu
+    while(true){
+        cout<<"MENU TIPOS"<<endl
+        <<"1.- Suma"<<endl
+        <<"2.- Multiplicacion"<<endl;
+        cout<<"Ingrese una opcion: ";
+        int opcion =0;
+        cin>>opcion;
+        if(opcion>=1 && opcion<= 2){
+                return opcion;
+        }
+		else{
+        	cout<<"La opcion elegida no es valida, ingrese una opcion entre 1 y 2"<<endl;
+        }
+ 	}//end del while
+        return 0;
+}//fin metodo menu
+
+void printR(){
 	cout<<"RACIONALES\n\n";
 	if (!vectores_R.empty())
 	{
@@ -149,7 +256,9 @@ void print(){
 	else{
 		cout<<"El vector esta vacio"<<endl;
 	}
+}
 
+void printC(){
 	cout<<"\nCOMPLEJOS\n\n";
 	if (!vectores_C.empty())
 	{
@@ -163,18 +272,16 @@ void print(){
 	}
 }
 
-int MCD(int n1, int n2) {
-    int resultado = 1; 
-    int factor = 2;    
 
-    while(factor <= n1 || factor <= n2) {
-        while(!(n1 % factor) && !(n2 % factor)) {
-            resultado *= factor;
-            n1 /= factor;
-            n2 /= factor;
-        }
-        if(factor == 2) factor++; 
-        else factor+=2;            
-    }
-    return resultado;
-}
+int MCD(int a, int b){//inicio MCD
+	int r;
+	if(b==0){
+		return a;
+	}
+	else{
+		r=a%b;
+		a=b;
+		b=r;
+		return MCD(a,b);
+	}
+}//fin MCD
